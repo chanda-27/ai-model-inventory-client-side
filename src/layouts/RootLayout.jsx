c6208512fdffeea73c987e929016e521688dc4ca
parthopauldev <parthopaul.dev@gmail.com>
2025-11-09 08:52:43 -0600

    added:react router

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <p>partho</p>
+        </div>
+    );
+};
+
+export default RootLayout;
