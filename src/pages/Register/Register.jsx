7cae21f8c837b71db6ca7e62d2092c62c56c9b90
parthopauldev <parthopaul.dev@gmail.com>
2025-11-10 23:02:30 -0600

    Implement password validation

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,36 +1,72 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 import Swal from "sweetalert2";
+import { Link, useNavigate } from "react-router";
 
 const Register = () => {
-  const { createUser,updateUser,user} = use(AuthContext);
+
+  const navigate = useNavigate();
+  const [passwordError, setPasswordError] = useState("");
+
+  const { createUser, updateUser, user,loginWithGoogle } = use(AuthContext);
   const handleRegister = (e) => {
     e.preventDefault();
 
-      const form = e.target;
-      let name = form.name.value;
+    const form = e.target;
+    let name = form.name.value;
     let photo = form.photo.value;
     const email = form.email.value;
     const password = form.password.value;
+    if (password.length < 6) {
+      return setPasswordError("Your password will be more than 6 Word");
+    } else if (!/[A-Z]/.test(password)) {
+      return setPasswordError(
+        "Your password must contain at least one uppercase letter."
+      );
+    } else if (!/[a-z]/.test(password)) {
+      return setPasswordError(
+        "Your password must contain at least one lowercase letter."
+      );
+    } else {
+      setPasswordError("");
+    }
     createUser(email, password)
-        .then((result) => {
-          updateUser({ ...user, displayName: name, photoURL: photo })
+      .then((result) => {
+        updateUser({ ...user, displayName: name, photoURL: photo });
         console.log(result.user);
-       Swal.fire({
-  position: "top-end",
-  icon: "success",
-  title: "Your Account has been Created",
-  showConfirmButton: false,
-  timer: 1500
-});
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your Account has been Created",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/");
       })
       .catch((err) => {
-          console.log(err.message);
-          Swal.fire(err.message);
-
+        console.log(err.message);
+        Swal.fire(err.message);
       });
   };
-
+let handleLoginWithGoogle = () => {
+  
+    loginWithGoogle()
+      .then(result => {
+      Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "You are logged in",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+     
+      navigate('/')
+    })
+      .catch(err => {
+     
+       Swal.fire(err.message);
+    })
+  }
   return (
     <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-5xl font-bold">Register now!</h1>
@@ -71,13 +107,24 @@ const Register = () => {
               className="input"
               name="password"
               placeholder="Password"
-            />
+                      />
+                       {
+                    passwordError && <p className='text-error'>{ passwordError}</p>
+                  }
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
             <button className="btn btn-neutral mt-4">Register</button>
           </fieldset>
-        </form>
+              </form>
+                          <button onClick={handleLoginWithGoogle} className="btn btn-outline btn-primary">Login With Google</button>
+
+                <p className="">
+            Allready  Have An Account ?{' '}
+            <Link className="text-secondary text-[17px]" to="/login">
+             Login
+                  </Link>
+                  </p>
       </div>
     </div>
   );
