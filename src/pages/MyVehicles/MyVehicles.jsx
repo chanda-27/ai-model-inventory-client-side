c3156f3880f243927fe7e132f0edc3026faaf1d7
parthopauldev <parthopaul.dev@gmail.com>
2025-11-11 02:59:41 -0600

    Feature: Delete product of user

--- src/pages/MyVehicles/MyVehicles.jsx
+++ src/pages/MyVehicles/MyVehicles.jsx
@@ -1,10 +1,12 @@
 import React, { use, useEffect, useState } from "react";
 import useAxiosSecure from "./../../hooks/useAxiosSecure";
 import { AuthContext } from "../../contexts/AuthContext";
-
+import Swal from "sweetalert2";
+import useAxios from "../../hooks/useAxios";
 const MyVehicles = () => {
   const axiosSecure = useAxiosSecure();
-  const { user } = use(AuthContext);
+    const { user } = use(AuthContext);
+    const axiosInstance = useAxios();
     const [products, setProducts] = useState([])
 
     useEffect(() => {
@@ -14,15 +16,49 @@ const MyVehicles = () => {
                 setProducts(data.data);
 
         })
-  }, [user,axiosSecure]);
+    }, [user, axiosSecure]);
+     const handleDeleteProduct = (_id) => {
+        
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+    
+                    axiosInstance.delete(`/products/${_id}`)
+                        .then(data => {
+                            if (data.data.deletedCount) {
+                                Swal.fire({
+                                    title: "Deleted!",
+                                    text: "Your bid has been deleted.",
+                                    icon: "success"
+                                });
+    
+                                // 
+                                const remainingProducts = products.filter(bid => bid._id !== _id);
+                                setProducts(remainingProducts)
+                            }
+                        })
+    
+    
+                }
+            });
+    }
     return <div>
         {
             products.map(product => (
-               <div>
-                   <p>{ product.vehicleName}</p> 
-                   <p>{ product.userEmail}</p> 
-                    
-               </div>
+              <div className='border'>
+            <p>{product.vehicleName}</p> 
+            <p>{product.userEmail}</p>
+            <button className='btn'>update</button>
+          
+            <button onClick={()=>handleDeleteProduct(product._id)} className='btn'>Delete</button>
+        </div>
             ))
       }
   </div>;
