03071923d82ee33e2ee2d49a964a58d35429ca53
parthopauldev <parthopaul.dev@gmail.com>
2025-11-11 02:25:37 -0600

    user product load on my vehicles page feature has been implemented

--- src/pages/MyVehicles/MyVehicles.jsx
+++ src/pages/MyVehicles/MyVehicles.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { use, useEffect, useState } from "react";
+import useAxiosSecure from "./../../hooks/useAxiosSecure";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const MyVehicles = () => {
-    return (
-        <div>
-            this is My Vehicles
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { user } = use(AuthContext);
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      
+        axiosSecure.get(`/userProducts?email=${user.email}`)
+            .then(data => {
+                setProducts(data.data);
+
+        })
+  }, [user,axiosSecure]);
+    return <div>
+        {
+            products.map(product => (
+               <div>
+                   <p>{ product.vehicleName}</p> 
+                   <p>{ product.userEmail}</p> 
+                    
+               </div>
+            ))
+      }
+  </div>;
 };
 
 export default MyVehicles;
